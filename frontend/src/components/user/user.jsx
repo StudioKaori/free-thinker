9d448fed4212cfc747c4a5558aed162b5ff22b2e
Kaori <studiokaori.com>
2020-11-24 19:34:14 +0100

    Add recoil

---
+++ frontend/src/components/user/user.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import Api from "../../api/Api";
+import { useRecoilState } from "recoil";
+import { userState } from "../../js/state-information";
+import Student from "../student/student";
+import Teacher from "../teacher/teacher";
+
+export default function User() {
+  const [status, setStatus] = useState(0);
+  const [user, setUser] = useRecoilState(userState);
+
+  // for user info
+  const getUser = () => {
+    Api.get("/user/loggedInUser").then((res) => setUser([res.data]));
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  useEffect(() => {
+    if (user.length !== 0) {
+      setStatus(1);
+    }
+  }, [user]);
+
+  return (
+    <div>
+      {status === 1 ? (
+        user[0].userType === "Teacher" ? (
+          <Teacher />
+        ) : (
+          <Student />
+        )
+      ) : null}
+    </div>
+  );
+}
---
+++ frontend/src/js/state-information.js
@@ -0,0 +1,6 @@
+const { atom } = require("recoil");
+
+export const userState = atom({
+  key: "userState",
+  default: [],
+});
