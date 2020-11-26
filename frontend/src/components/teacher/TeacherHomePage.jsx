5dfb843642c4cccf79d85730a3a6e5df5199c5d1
Kaori <studiokaori.com>
2020-11-26 11:31:30 +0100

    Add studentHomePage,Teacherhomepage

--- frontend/src/components/teacher/TeacherHomePage.jsx
+++ frontend/src/components/teacher/TeacherHomePage.jsx
@@ -1,12 +1,12 @@
 import { useRecoilState } from "recoil";
 import { userState } from "../../js/state-information";
 
-export default function Student() {
+export default function TeacherHomePage() {
   const [user, setUser] = useRecoilState(userState);
 
   return (
     <div>
-      <h4 className="card-title">student</h4>
+      <h4 className="card-title">Teacher</h4>
       {user[0].name}
     </div>
   );
