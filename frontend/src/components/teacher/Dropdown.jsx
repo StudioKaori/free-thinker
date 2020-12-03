e0a335bfadd073eeb9fa6c476891d0fc17e8461b
Sreeja Menon <sreejam@gmail.com>
2020-12-03 10:15:08 +0100

    Added basic contents of teacher home page

---
+++ frontend/src/components/teacher/Dropdown.jsx
@@ -0,0 +1,16 @@
+
+
+
+import React from "react";
+
+export default function Dropdown(){
+    
+     
+    return(
+        <div>
+                <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown"> Theme for the day </button>
+                 
+                
+        </div>
+    );
+}
