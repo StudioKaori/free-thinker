bec2d96134635fc17bd5cbb7abe7b7842e024d22
Alaa <alaa.kh.zaza@gmail.com>
2020-12-03 21:57:00 +0100

    add lecture list to student in navbar

---
+++ frontend/src/components/lecture/Lecture.js
@@ -0,0 +1,16 @@
+import {React} from 'react';
+import ReactHtmlParser from "react-html-parser"
+
+
+export default function Lecture(props) {
+    return(
+        <div className ="card-body">
+            <div className ="card-title font-weight-bold">
+                {props.lecture.title}
+            </div>
+            <p class="card-text">
+            {ReactHtmlParser(props.lecture.body)} 
+            </p>
+        </div>
+    );
+}
