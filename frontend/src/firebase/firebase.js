c2b6bafaf67d63aef51389b89a7701e2cd8c5be9
Kaori <67104053+StudioKaori@users.noreply.github.com>
2020-12-03 09:39:36 +0100

    Update firebase.js
    Delete api key

--- frontend/src/firebase/firebase.js
+++ frontend/src/firebase/firebase.js
@@ -2,13 +2,8 @@ import firebase from "firebase/app";
 import "firebase/storage";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyCVpGp46CRTWLDCsKDpPHn0wNoIaheKTLg",
-  authDomain: "free-thinker-d732f.firebaseapp.com",
-  databaseURL: "https://free-thinker-d732f.firebaseio.com",
-  projectId: "free-thinker-d732f",
-  storageBucket: "free-thinker-d732f.appspot.com",
-  messagingSenderId: "334017849826",
-  appId: "1:334017849826:web:2c57956e80724f79d771bb",
+  //put your apikey infor
+
 };
 
 // Initialize Firebase
