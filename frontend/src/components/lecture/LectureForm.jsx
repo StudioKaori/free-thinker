b7c34b7bb0c664ae7fae1b56d0e85345a167e09b
Kaori <studiokaori.com>
2020-12-09 15:30:34 +0100

    Small bug fix

--- frontend/src/components/lecture/LectureForm.jsx
+++ frontend/src/components/lecture/LectureForm.jsx
@@ -5,7 +5,6 @@ import "react-datepicker/dist/react-datepicker.css";
 import moment from "moment";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
-import moment from "moment";
 
 export default function LectureForm({ onCreateClick, dateFromCalDate }) {
   const [title, setTitle] = useState("");
@@ -64,7 +63,7 @@ export default function LectureForm({ onCreateClick, dateFromCalDate }) {
 
           <div className="form-group">
             <label>Unlock Date:</label>
-             {/* <input
+            {/* <input
               id="lectureDateInLectureForm"
               type="text"
               className="form-control"
@@ -72,17 +71,16 @@ export default function LectureForm({ onCreateClick, dateFromCalDate }) {
               value={unlockDate}
               onChange={(e) => setUnlockDate(e.target.value)}
             /> */}
-          <div>
-          <DatePicker
-            id="lectureDateInLectureForm"
-            className="form-control"
-            selected={unlockDate}
-            dateFormat='yyyy-MM-dd'
-            onChange={(e) => setUnlockDate(e)}
-             />
+            <div>
+              <DatePicker
+                id="lectureDateInLectureForm"
+                className="form-control"
+                selected={unlockDate}
+                dateFormat="yyyy-MM-dd"
+                onChange={(e) => setUnlockDate(e)}
+              />
             </div>
-            </div>
-            
+          </div>
 
           <div className="form-group">
             <label>Unlock Time:</label>
@@ -125,11 +123,9 @@ export default function LectureForm({ onCreateClick, dateFromCalDate }) {
                 setUnlockDate("");
                 setUnlockTime("");
                 setYoutube("");
-                document.getElementsByClassName("ck-content")[0].childNodes[0].innerHTML = '';
-
-
-
-
+                document.getElementsByClassName(
+                  "ck-content"
+                )[0].childNodes[0].innerHTML = "";
               }}
             >
               Create
