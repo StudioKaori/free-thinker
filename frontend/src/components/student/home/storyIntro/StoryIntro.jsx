5d0ce98a7eff783334a6e0ddcf263c24da2f5f68
Kaori <studiokaori.com>
2020-12-10 10:03:44 +0100

    Add animation for story

--- frontend/src/components/student/home/storyIntro/StoryIntro.jsx
+++ frontend/src/components/student/home/storyIntro/StoryIntro.jsx
@@ -2,24 +2,22 @@ import "../../../../css/student/storyIntro/storyIntro.css";
 
 export default function StoryIntro() {
   return (
-    <div>
-      <div className="storyIntroElement fadeIn delay1">
+    <div className="storyIntro">
+      <div className="storyIntroElement fadeIn">
         This is the story of parallel world...
       </div>
-      <div className="storyIntroElement fadeIn delay2">AD 2080</div>
-      <div className="storyIntroElement fadeIn delay3">
+      <div className="storyIntroElement fadeIn">AD 2080</div>
+      <div className="storyIntroElement fadeIn">
         Climate change hit the world
       </div>
-      <div className="storyIntroElement fadeIn delay4">
-        Many cities were lost
-      </div>
-      <div className="storyIntroElement fadeIn delay5">Your mission is</div>
+      <div className="storyIntroElement fadeIn">Many cities were lost</div>
+      <div className="storyIntroElement fadeIn">Your mission is</div>
       <div className="storyIntroElement storyIntroLast">
-        <span class="animate__animated animate__bounce  animate__delay-3s">
+        <div class="animate__animated animate__bounce animate__delay-3s">
           Find the lost cities
-        </span>
-        <span>and...</span>
-        <span>Hunt the Monsters!!</span>
+        </div>
+        <div>and...</div>
+        <div>Hunt the Monsters!!</div>
       </div>
     </div>
   );
