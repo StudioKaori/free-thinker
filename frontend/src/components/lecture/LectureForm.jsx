73cda46165eb521642ce897b16cb8029cbfc0fc7
Sreeja Menon <sreejam@gmail.com>
2020-12-11 13:20:15 +0100

    lecture form

--- frontend/src/components/lecture/LectureForm.jsx
+++ frontend/src/components/lecture/LectureForm.jsx
@@ -4,6 +4,7 @@ import "react-datepicker/dist/react-datepicker.css";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import PopUpMsg from "../assignment/PopUpMsg";
+import moment from "moment";
 
 import 'react-datepicker/dist/react-datepicker.css';
 
