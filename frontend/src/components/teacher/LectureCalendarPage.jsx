efcb538f72bfacdb42a5f25e9deda905ed1e9d9b
StudioKaori <mkaori.1101@gmail.com>
2020-12-14 13:56:00 +0100

    Adjust css for lecture calendar

--- frontend/src/components/teacher/LectureCalendarPage.jsx
+++ frontend/src/components/teacher/LectureCalendarPage.jsx
@@ -3,9 +3,7 @@ import LectureCalendar from "./schedule/LectureCalendar";
 export default function LectureCalendarPage() {
   return (
     <div className="body-wrapper">
-      <div className="lecture-calendar-wrapper">
-        <LectureCalendar />
-      </div>
+      <LectureCalendar />
     </div>
   );
 }
