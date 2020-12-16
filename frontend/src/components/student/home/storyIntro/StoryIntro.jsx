d3304b86ea1273d10b96ff0ac91a787fffe0762e
StudioKaori <mkaori.1101@gmail.com>
2020-12-16 13:50:23 +0100

    Add intro anime

--- frontend/src/components/student/home/storyIntro/StoryIntro.jsx
+++ frontend/src/components/student/home/storyIntro/StoryIntro.jsx
@@ -4,20 +4,26 @@ import "../../../../css/student/storyIntro/storyIntro.css";
 export default function StoryIntro() {
   return (
     <div className="storyIntro">
-      <div className="storyIntroElement fadeOut welcome">
+      <div className="storyIntroElement fadeOutAnime welcome">
         Welcome to
         <img src="/assets/img/logo.svg" alt="Free Thinker" />
       </div>
-      <div className="storyIntroElement fadeIn">AD 2280</div>
-      <div className="storyIntroElement fadeIn">
+      <div className="storyIntroElement fadeInAnime">AD 2280</div>
+      <div className="storyIntroElement fadeInAnime">
         Climate change hit the world
       </div>
-      <div className="storyIntroElement fadeIn">Many cities were lost</div>
-      <div className="storyIntroElement fadeIn">Your mission is</div>
-      <div className="storyIntroElement fadeIn">
+      <div className="storyIntroElement fadeInAnime">Many cities were lost</div>
+      <div className="storyIntroElement fadeInAnime">Your mission is...</div>
+      <div className="storyIntroElement fadeInAnime fadeInAnimeSlower">
         <div>Find the lost cities</div>
         <div>and...</div>
-        <div>Hunt the Monsters!!</div>
+        <div>Monsters!!</div>
+        <div className="story-monsters-img">
+          <img
+            src="/assets/img/css/intro-story/introstory-monsters.jpg"
+            alt="monsters"
+          />
+        </div>
       </div>
     </div>
   );
