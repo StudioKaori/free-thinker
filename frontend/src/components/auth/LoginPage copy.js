1f39a49a1513360d0eb5a36c78d52c91595812f3
StudioKaori <mkaori.1101@gmail.com>
2020-12-17 15:22:58 +0100

    Change login page

---
+++ frontend/src/components/auth/LoginPage copy.js
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import { Redirect } from "react-router-dom";
+import Auth from "../../services/Auth";
+import LoginForm from "./LoginForm";
+import RegisterForm from "./RegisterForm";
+
+function LoginPage() {
+
+    const [loginOpen, setLoginOpen] = useState(false);
+    const [teacherRegisterOpen, setTeacherRegisterOpen] = useState(false);
+    const [studentRegisterOpen, setStudentRegisterOpen] = useState(false);
+
+    const login = async (loginData) => {
+        const loginSuccess = await Auth.login(loginData);
+        if (!loginSuccess) {
+            alert("Invalid credentials");
+        }
+    };
+
+    const register = async (registrationData) => {
+        const registerSuccess = await Auth.register(registrationData);
+        if (!registerSuccess) {
+            alert("Couldn't register check credentials and try again");
+        }
+
+        if (registrationData.userType === 1) {
+            return <Redirect to="/teacher" />;
+        } else if (registrationData.userType === 2) {
+            return <Redirect to="/student" />;
+        }
+    };
+
+    return (
+        <div className="wrapper login-section-1">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6 mt-4 text-center">
+                        <h1>Free Thinkers</h1>
+                    </div>
+
+                    <div className="col-md-6 mt-4">
+                        <ul className="nav d-flex justify-content-sm-center justify-content-md-end">
+                            <li 
+                                className={`nav-item btn btn-info m-1 ${loginOpen && 'active'}`}
+                                onClick={() => {
+                                    setLoginOpen(!loginOpen);
+                                    setTeacherRegisterOpen(false);
+                                    setStudentRegisterOpen(false);
+                                }}
+                            >Login</li>
+                            <li 
+                                className={`nav-item btn btn-info m-1 ${teacherRegisterOpen && 'active'}`}
+                                onClick={() => {
+                                    setLoginOpen(false);
+                                    setTeacherRegisterOpen(!teacherRegisterOpen);
+                                    setStudentRegisterOpen(false);
+                                }}
+                            >Teacher Sign Up</li>
+                            <li 
+                                className={`nav-item btn btn-info m-1 ${studentRegisterOpen && 'active'}`}
+                                onClick={() => {
+                                    setLoginOpen(false);
+                                    setTeacherRegisterOpen(false);
+                                    setStudentRegisterOpen(!studentRegisterOpen);
+                                }}
+                            >Student Sign Up</li>
+                        </ul>
+                        <div className="row">
+                            { loginOpen &&
+                                <div className="col-12  strong-shadow">
+                                    <LoginForm onSubmit={login} />
+                                </div>
+                            }
+
+                            { teacherRegisterOpen &&
+                                <div className="col-12">
+                                    <RegisterForm userType="Teacher" onSubmit={register} />
+                                </div>
+                            }
+
+                            { studentRegisterOpen &&
+                                <div className="col-12">
+                                    <RegisterForm userType="Student" onSubmit={register} />
+                                </div>
+                            }
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default LoginPage;
--- frontend/src/components/home/HomePage.jsx
+++ frontend/src/components/home/HomePage.jsx
@@ -7,67 +7,73 @@ import Footer from "./Footer";
 // =====================================================================
 // Content in Home / Login page
 function HomePage() {
+  return (
+    <>
+      <div className="parallax-bg login-bg-02">
+        <div className="login-contents-wrapper">
+          <div>
+            <TeacherCarousel />
+            <h3 className="mb-3">Teachers</h3>
+            <p>
+              Plan your daily routine in advance and follow your students'
+              activities and results.
+              <br />
+              Using this platform, you can offer your students a wonderful
+              journey into learning.
+              <br />
+              Enjoy a professional tool for yourself and an exicting environment
+              for your pupils.
+            </p>
+            <p>Your creativity is the only limit!</p>
+          </div>
 
-    return (
-        <>
-            <section className="home-section">
-                <div className="row d-flex flex-wrap-reverse">
-                    <div className="col-md-6 bg-dark home-carousel">
-                        <TeacherCarousel />
-                    </div>
-                    <div className="col-md-6 bg-light home-infos">
-                        <h3 className="mb-3">Teachers</h3>
-                        <p>
-                            Plan your daily routine in advance and
-                            follow your students' activities and results.
-                            <br />
-                            Using this platform, you can
-                            offer your students a wonderful journey into learning.
-                            <br />
-                            Enjoy a professional tool for yourself and an exicting
-                            environment for your pupils.
-                        </p>
-                        <p>Your creativity is the only limit!</p>
-                    </div>
-                </div>
-            </section>
-            <section className="home-section">
-                <div className="row">
-                    <div className="col-md-6 bg-light home-infos">
-                        <h3 className="mb-3">Parents</h3>
-                        <p>
-                            Almost like they were in a real classroom, children
-                            can do their assignments, follow daily lectures or
-                            attend live sessions.
-                            <br />
-                            They will learn on a secure and amazing online platform
-                            where it is possible to associate fun with knowledge.
-                            <br />
-                            
-                        </p>
-                        <p>Let children be in good hands.</p>
-                    </div>
-                    <div className="col-md-6 bg-dark home-carousel">
-                        <StudentCarousel />
-                    </div>
-                </div>
-            </section>
-            <section className="about-section">
-                <h3 className="mb-3">Who are we ?</h3>
-                <p>
-                    A team passionate about learning and development and
-                    this is the best of the two worlds. 
-                    <br />
-                    We are interested in your feedback and suggestions.
-                    Feel free to write to us at <em>freethinkers@mail.com</em>
-                </p>
-                <p>We are Free Thinkers !</p>
-            </section>
-            <Footer />
+          <div>
+            <StudentCarousel />
+            <h3 className="mb-3">Parents</h3>
+            <p>
+              Almost like they were in a real classroom, children can do their
+              assignments, follow daily lectures or attend live sessions.
+              <br />
+              They will learn on a secure and amazing online platform where it
+              is possible to associate fun with knowledge.
+              <br />
+            </p>
+            <p>Let children be in good hands.</p>
+          </div>
+        </div>
+      </div>
 
-        </>
-    )
+      <div className="login-who">
+        <h3 className="mb-3">Who are we ?</h3>
+        <p>
+          A team passionate about learning and development and this is the best
+          of the two worlds.
+          <br />
+          We are interested in your feedback and suggestions. Feel free to write
+          to us at <em>freethinkers@mail.com</em>
+        </p>
+        <p>We are Free Thinkers !</p>
+      </div>
+      <div className="login-footer">
+        <Footer />
+      </div>
 
+      {/* <div className="parallax-bg login-bg-03">
+        <StudentCarousel />
+        <h3 className="mb-3">Who are we ?</h3>
+        <p>
+          A team passionate about learning and development and this is the best
+          of the two worlds.
+          <br />
+          We are interested in your feedback and suggestions. Feel free to write
+          to us at <em>freethinkers@mail.com</em>
+        </p>
+        <p>We are Free Thinkers !</p>
+      </div> */}
+
+      {/* <Footer /> */}
+    </>
+  );
 }
 
 export default HomePage;
