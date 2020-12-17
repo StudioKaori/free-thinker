e749042e7a7d385e93523df66376a68aef4f2868
StudioKaori <mkaori.1101@gmail.com>
2020-12-17 07:06:45 +0100

    Add monsters json file

--- frontend/src/components/assignment/StudentAssignmentPage.jsx
+++ frontend/src/components/assignment/StudentAssignmentPage.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import moment from "moment";
 
@@ -12,124 +12,135 @@ import StudentQuiz from "./quiz/StudentQuiz";
 import AssignmentApi from "../../api/AssignmentApi";
 import AssignmentProgressApi from "../../api/AssignmentProgressApi";
 
-import "../../css/student/assignmentPage.css";
+import Congrats from "../student/assignment/Congrats";
 
+import "../../css/student/assignmentPage.css";
 
 // =====================================================================
 // Student assignment entry point, fetch assignment by id and display the related component
 export default function StudentAssignmentPage({ match }) {
+  // Variables
+  const assignmentId = Number(match.match.params.id);
+  const [assignment, setAssignment] = useState({});
+  const [isLastAssignment, setIsLastAssignment] = useState(false);
+  const [allDone, setAllDone] = useState(false);
+  const [end, setEnd] = useState(false); // Check for assignment completion
+  const [user] = useRecoilState(userState);
+  const [date] = useState(moment().format("yyyy-MM-DD"));
 
-    // Variables
-    const assignmentId = Number(match.match.params.id);
-    const [assignment, setAssignment] = useState({});
-    const [isLastAssignment, setIsLastAssignment] = useState(false);
-    const [allDone, setAllDone] = useState(false);
-    const [end, setEnd] = useState(false); // Check for assignment completion
-    const [user] = useRecoilState(userState);
-    const [date] = useState(moment().format("yyyy-MM-DD"));
+  const monster = useRef();
+  monster.current = "oni";
 
-    useEffect(() => {
-        // Current assignment
-        AssignmentApi.getAssignmentById(assignmentId).then((res) => {
-            setAssignment(res.data);
-            const done =
-                res.data.isDoneByUser.filter((usr) => usr.id === user[0].id).length > 0;
-            setEnd(done);
+  useEffect(() => {
+    // Current assignment
+    AssignmentApi.getAssignmentById(assignmentId).then((res) => {
+      setAssignment(res.data);
+      const done =
+        res.data.isDoneByUser.filter((usr) => usr.id === user[0].id).length > 0;
+      setEnd(done);
+    });
+
+    // Is it the last of the day ?
+    AssignmentApi.getAssignmentsByUnlockDate(date).then((res) => {
+      const lastAssignmentId = res.data[res.data.length - 1].id;
+      if (lastAssignmentId === assignmentId) {
+        setIsLastAssignment(true);
+      } else {
+        setAllDone(false); // In case we add an assignment the same day.
+      }
+    });
+  }, []);
+
+  // When/If assignment is done
+  useEffect(() => {
+    if (end) {
+      let updatedAssign = { ...assignment };
+      if (
+        updatedAssign.isDoneByUser.filter((usr) => usr.id === user[0].id)
+          .length > 0
+      ) {
+        // Means : student click on an already finished assignment
+        return;
+      }
+      const temp = updatedAssign.isDoneByUser.concat(user[0]);
+      updatedAssign.isDoneByUser = temp;
+
+      AssignmentApi.updateAssignment(updatedAssign).then(() =>
+        console.log("assignment", updatedAssign.id, "is done")
+      );
+
+      if (isLastAssignment && !allDone) {
+        // Send signal that all assignment for the day are done
+
+        AssignmentProgressApi.getAllAssignmentProgresss().then((res) => {
+          const todayStudentProgress = res.data.filter(
+            (prog) =>
+              prog.classDailySetting.date.substr(0, 10) === date &&
+              prog.student.id === user[0].id
+          )[0]; // Supposed to return an array of 1 element
+
+          const newObj = { ...todayStudentProgress };
+          newObj.assignmentsOfTheDayIsDone = true;
+
+          AssignmentProgressApi.updateAssignmentProgress(newObj).then((res) => {
+            console.log("End of the day, youhou!");
+            console.log("res", res.data);
+            monster.current = res.data.monster;
+          });
         });
+        setAllDone(true);
+      }
+    }
 
-        // Is it the last of the day ?
-        AssignmentApi.getAssignmentsByUnlockDate(date).then((res) => {
-            const lastAssignmentId = res.data[res.data.length - 1].id;
-            if (lastAssignmentId === assignmentId) {
-                setIsLastAssignment(true);
-            } else {
-                setAllDone(false); // In case we add an assignment the same day.
-            }
-        });
-    }, []);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [end, assignment]);
 
-    // When/If assignment is done
-    useEffect(() => {
-        if (end) {
-            let updatedAssign = { ...assignment };
-            if (
-                updatedAssign.isDoneByUser.filter((usr) => usr.id === user[0].id)
-                    .length > 0
-            ) {
-                // Means : student click on an already finished assignment
-                return;
-            }
-            const temp = updatedAssign.isDoneByUser.concat(user[0]);
-            updatedAssign.isDoneByUser = temp;
+  return (
+    <section className="body-wrapper">
+      <h2 className="border-bottom mb-3">{assignment.title}</h2>
 
-            AssignmentApi.updateAssignment(updatedAssign).then(() =>
-                console.log('assignment', updatedAssign.id, "is done")
-            );
+      {allDone && (
+        <div>
+          <Congrats monster={monster} />
+        </div>
+      )}
 
-            if (isLastAssignment && !allDone) { // Send signal that all assignment for the day are done
-
-                AssignmentProgressApi.getAllAssignmentProgresss()
-                    .then((res) => {
-                        const todayStudentProgress = res.data
-                            .filter(prog => prog.classDailySetting.date.substr(0,10) === date 
-                                && prog.student.id === user[0].id)[0]; // Supposed to return an array of 1 element
-
-                        const newObj = { ...todayStudentProgress }
-                        newObj.assignmentsOfTheDayIsDone = true;
-                        
-                        AssignmentProgressApi.updateAssignmentProgress(newObj)
-                            .then((res) => {
-                                console.log("End of the day, youhou!")
-                            });
-                })
-                setAllDone(true);
-            }
-        }
-
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [end, assignment]);
-
-    return (
-        <section className="body-wrapper">
-            <h2 className="border-bottom mb-3">{assignment.title}</h2>
-
-            {end ? (
-                <div className="text-center">
-                    <p>Thank you very much your teacher will review your answers.</p>
-                    <Link to="/" className="btn btn-danger">
-                        Back
+      {end ? (
+        <div className="text-center">
+          <p>Thank you very much your teacher will review your answers.</p>
+          <Link to="/" className="btn btn-danger">
+            Back
           </Link>
-                </div>
-            ) : assignment.type === "Chat" ? (
-                <StudentChat
-                    key={assignment.id}
-                    chat={JSON.parse(assignment.assignment)}
-                    setEnd={setEnd}
-                />
-            ) : assignment.type === "Speech" ? (
-                <StudentSpeech
-                    key={assignment.id}
-                    speech={JSON.parse(assignment.assignment)}
-                    setEnd={setEnd}
-                />
-            ) : assignment.type === "Quiz" ? (
-                <StudentQuiz
-                    key={assignment.id}
-                    quiz={JSON.parse(assignment.assignment)}
-                    setEnd={setEnd}
-                />
-            ) : assignment.type === "None" ? (
-                <div>
-                    {assignment.title}
-                    {assignment.instructions}
-                    <div className="text-right">
-                        <button
-                            className="btn btn-success"
-                            onClick={() => setEnd(true)}
-                        >I am done</button>
-                    </div>
-                </div>
-            ) : null}
-        </section>
-    );
+        </div>
+      ) : assignment.type === "Chat" ? (
+        <StudentChat
+          key={assignment.id}
+          chat={JSON.parse(assignment.assignment)}
+          setEnd={setEnd}
+        />
+      ) : assignment.type === "Speech" ? (
+        <StudentSpeech
+          key={assignment.id}
+          speech={JSON.parse(assignment.assignment)}
+          setEnd={setEnd}
+        />
+      ) : assignment.type === "Quiz" ? (
+        <StudentQuiz
+          key={assignment.id}
+          quiz={JSON.parse(assignment.assignment)}
+          setEnd={setEnd}
+        />
+      ) : assignment.type === "None" ? (
+        <div>
+          {assignment.title}
+          {assignment.instructions}
+          <div className="text-right">
+            <button className="btn btn-success" onClick={() => setEnd(true)}>
+              I am done
+            </button>
+          </div>
+        </div>
+      ) : null}
+    </section>
+  );
 }
